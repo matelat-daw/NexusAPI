a6cbddf1d6adb643a4a4643dfa5df3d1b8ae0dd2
Orion68 <orions68@gmail.com>
2025-06-10 00:33:56 +0100

    Update 10-6-2025 0:33

--- src/main/java/com/futureprograms/NexusAPI/interfaces/UserRepository.java
+++ src/main/java/com/futureprograms/NexusAPI/interfaces/UserRepository.java
@@ -1,9 +1,7 @@
-// src/main/java/com/futureprograms/NexusAPI/repository/UserRepository.java
-package com.futureprograms.NexusAPI.interfaces;
+package com.futureprograms.NexusAPI.interfaces.primary;
 
 import com.futureprograms.NexusAPI.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 
 public interface UserRepository extends JpaRepository<User, String> {
     User findByEmail(String email);
